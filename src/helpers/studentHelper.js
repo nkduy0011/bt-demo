57647874f05398a9e17e1e0a1d6901183096f687
Nguyễn Khắc Duy <duynk@ezcloud.vn>
2024-05-29 11:42:13 +0700

    rm code

--- src/helpers/studentHelper.js
+++
@@ -1,8 +0,0 @@
-export const calculateAge = (birthday) => {
-  // birthday is a date
-  if (birthday) {
-    var ageDifMs = Date.now() - new Date(birthday).getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getFullYear() - 1970);
-  } else return null;
-};
--- src/services/localStorageService.js
+++
@@ -1,8 +0,0 @@
-export const getDataKey = (key) => {
-  let stringData = localStorage.getItem(key);
-  return JSON.parse(stringData);
-};
-export const setData = (key, data) => {
-  let stringData = JSON.stringify(data);
-  localStorage.setItem(key, stringData);
-};
